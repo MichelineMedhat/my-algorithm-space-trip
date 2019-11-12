eb8ca8304570210a300f089ec27956575b858f73
MichelineMedhat <michelinemedhat@gmail.com>
2019-11-12 02:20:42 +0200

    Find the Root

---
+++ Divide-and-Conquer/Find the Root/FindTheRoot/FindTheRoot/FindTheRoot.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FindTheRoot
+{
+    class FindTheRoot
+    {
+        //========================================================================================================
+        //GIVEN CODE:
+        //===========
+        // Calculate the function at a given x (i.e. f(x))
+        static double f(int p, int q, int r, int s, int t, int u, double x)
+        {
+            return p * Math.Exp(-x) + q * Math.Sin(x) + r * Math.Cos(x) + s * Math.Tan(x) + t * x * x + u;
+        }
+
+        // "eps" a very small value
+        const double eps = 1e-9;
+
+
+        //========================================================================================================
+        //YOUR CODE IS HERE:
+        //==================
+        // Write efficient algorithm to get the value of x that makes the function f(x) = 0 
+        // Return -1 if x is not exists
+        // HINT: you can consider that the root (x) is found if the function value f(x) is: -eps < F(x) < eps
+
+
+        /// <summary>
+        /// Write efficient algorithm to get the equation's root (if any) of function f
+        /// </summary>
+        /// <param name="p"> int between [0, 20] </param>
+        /// <param name="q"> int between [-20, 0] </param>
+        /// <param name="r"> int between [0, 20] </param>
+        /// <param name="s"> int between [-20, 0]</param>
+        /// <param name="t"> int between [-20, 0]</param>
+        /// <param name="u"> int between [0, 20] </param>
+        /// <returns>value of x if exists. Else, return -1</returns>
+        public static double findTheRoot(int p, int q, int r, int s, int t, int u)
+        {
+            //Write your code HERE
+            return binarySearch(p, q, r, s, t, u, 0, 1);
+        }
+
+        static double binarySearch(int p, int q, int r, int s, int t, int u, double start, double end)
+        {
+            double mid = (double)(start + end) / 2;
+
+            double fx = f(p, q, r, s, t, u, mid);
+            if (fx > -eps && fx < eps)
+            {
+                return mid;
+            }
+            if (mid == start || mid == end)
+            {
+                return -1;
+            }
+            else if (fx < eps)
+            {
+                return binarySearch(p, q, r, s, t, u, start, mid);
+            }
+            else
+            {
+                return binarySearch(p, q, r, s, t, u, mid, end);
+            }
+        }
+
+
+
+
+        //========================================================================================================
+
+        #region Sorting function - if you need it
+        static void Sorting(int[] collection, int N)
+        {
+            QSort(0, N - 1, collection, N);
+        }
+
+        static void Swap(int i, int j, int[] collection)
+        {
+            int tmp = collection[i];
+            collection[i] = collection[j];
+            collection[j] = tmp;
+        }
+
+        static void QSort(int startIndex, int finalIndex, int[] collection, int N)
+        {
+            if (startIndex >= finalIndex) return;
+            int i = startIndex, j = finalIndex;
+            while (i <= j)
+            {
+                while (i < N && collection[startIndex] >= collection[i]) i++;
+                while (j > -1 && collection[startIndex] < collection[j]) j--;
+                if (i <= j)
+                {
+                    Swap(i, j, collection);
+                }
+            }
+            Swap(startIndex, j, collection);
+            QSort(startIndex, j - 1, collection, N);
+            QSort(i, finalIndex, collection, N);
+        }
+        #endregion
+    }
+}
---
+++ Divide-and-Conquer/Find the Root/FindTheRoot/FindTheRoot/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace FindTheRoot
+{
+    class Program
+    {
+
+        #region I/O
+        static void Main(string[] args)
+        {
+            int nCases;
+            double result, actualResult;
+            StreamReader sr;
+            TextReader origConsole = Console.In;
+            Console.WriteLine("Find The Root Problem:\n[1] Sample test cases\n[2] Complete testing");
+            Console.Write("\nEnter your choice [1-2]: ");
+            char choice = (char)Console.ReadLine()[0];
+            switch (choice)
+            {
+                case '1':
+                        sr = new StreamReader("samples.txt");
+                        Console.SetIn(sr);
+
+                        nCases = int.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < nCases; i++)
+                        {
+                            string[] lineItems = Console.ReadLine().Split(' ');
+                            int p = int.Parse(lineItems[0]);
+                            int q = int.Parse(lineItems[1]);
+                            int r = int.Parse(lineItems[2]);
+                            int s = int.Parse(lineItems[3]);
+                            int t = int.Parse(lineItems[4]);
+                            int u = int.Parse(lineItems[5]);
+
+                            Console.WriteLine("Case " + (i + 1).ToString() + ":");
+                            Console.WriteLine("p = " + p + ", q = " + q + ", r = " + r + ", s = " + s + ", t = " + t + ", u = " + u);
+
+                            long timeBefore = System.Environment.TickCount;
+                            result = Math.Round(FindTheRoot.findTheRoot(p, q, r, s, t, u), 4);
+                            long timeAfter = System.Environment.TickCount;
+                            if (timeAfter - timeBefore > 110)
+                            {
+                                Console.WriteLine("Time limit exceed: case # " + (i + 1));
+                                sr.Close();
+                                return;
+                            }
+                            actualResult = double.Parse(Console.ReadLine());
+                            if (actualResult != result)
+                            {
+                                Console.WriteLine("Wrong Answer!" + " your answer = "
+                                    + result + ", correct answer = " + actualResult);
+                                sr.Close();
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Succeed");
+                            }
+                        }
+                        sr.Close();
+
+                        Console.SetIn(origConsole);
+
+                        Console.WriteLine("Sample cases run successfully. you should run Complete Testing... ");
+                        Console.Write("Do you want to run Complete Testing now (y/n)? ");
+                        choice = (char)Console.Read();
+                        if (choice == 'n' || choice == 'N')
+                            break;
+                        else if (choice == 'y' || choice == 'Y')
+                            goto CompleteTest;
+                        else
+                        {
+                            Console.WriteLine("Invalid Choice!");
+                            break;
+                        }
+                case '2':
+
+                    CompleteTest:
+                        Console.WriteLine("\nComplete Testing is running now...");
+                        sr = new StreamReader("input.txt");
+                        Console.SetIn(sr);
+                   
+                        nCases = int.Parse(Console.ReadLine());
+
+                        for (int i = 0; i < nCases; i++)
+                        {
+                            string[] lineItems = Console.ReadLine().Split(' ');
+                            int p = int.Parse(lineItems[0]);
+                            int q = int.Parse(lineItems[1]);
+                            int r = int.Parse(lineItems[2]);
+                            int s = int.Parse(lineItems[3]);
+                            int t = int.Parse(lineItems[4]);
+                            int u = int.Parse(lineItems[5]);
+
+                            long timeBefore = System.Environment.TickCount;
+                            result = Math.Round(FindTheRoot.findTheRoot(p, q, r, s, t, u), 4); 
+                            long timeAfter = System.Environment.TickCount;
+                            if (timeAfter - timeBefore > 100)
+                            {
+                                Console.WriteLine("Time limit exceed: case # " + (i + 1));
+                                sr.Close();
+                                return;
+                            }
+                            actualResult = double.Parse(Console.ReadLine());
+                                if (actualResult != result)
+                            {
+                                Console.WriteLine("Wrong Answer: case # " + (i + 1) + ": your answer = "
+                                    + result + ", correct answer = " + actualResult);
+                                sr.Close();
+                                return;
+                            }
+                        }
+                        sr.Close();
+                        Console.WriteLine("\nCongratulations... your program runs successfully");
+                        break;
+                    
+            }
+        }
+        #endregion
+        
+    }
+}
