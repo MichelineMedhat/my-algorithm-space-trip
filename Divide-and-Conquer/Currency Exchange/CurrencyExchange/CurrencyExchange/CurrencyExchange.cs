9c3985d23e3a1a6fb7202a5758326bd2cb323094
MichelineMedhat <michelinemedhat@gmail.com>
2019-11-12 02:16:17 +0200

    Curreny Exchange

---
+++ Divide-and-Conquer/Currency Exchange/CurrencyExchange/CurrencyExchange/CurrencyExchange.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    // *****************************************
+    // DON"T CHANGE CLASS NAME OR FUNCTION NAME
+    // *****************************************
+    public static class CurrencyExchange
+    {
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// find the index of the USD Dollar $ with the smallest number of questions.
+        /// </summary>
+        /// <param name="N">Number of customers (N)</param>
+        /// <param name="M">Number of currencies (M)</param>
+        /// <param name="knows">N*M Matrix indicating whether customer i knows currency j or not</param>
+        /// <returns>index of US Dollar</returns>
+        public static int CheckUSD(int N, int M, bool[,] knows)
+        {
+            //knows[P,C]=true if person P knows currency C and knows[P,C]=false if person P doesn't know Currency C.
+            int thisIsTheOne = 0;
+            int col = 0;
+            int counter = 0;
+
+            while (col < M)
+            {
+
+                for (int i = 0; i < N; i++)
+                {
+                    if (knows[i, col])
+                    {
+                        counter += 1;
+                        if (counter == N)
+                        {
+                            thisIsTheOne = col;
+                            col = M;
+                            break;
+                        }
+                    }
+
+                    else
+                    {
+                        while (true)
+                        {
+                            col += 1;
+                            if (knows[i, col])
+                            {
+                                counter = 0;
+                                break;
+                            }
+                            else
+                                continue;
+                        }
+                        break;
+                    }
+                }
+
+            }
+            return thisIsTheOne;
+        }
+    }
+}
---
+++ Divide-and-Conquer/Currency Exchange/CurrencyExchange/CurrencyExchange/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CurrencyExchange
+{
+	class Program
+	{
+ 
+        #region I/O
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Currency Exchange Problem:\n[1] Sample test cases\n[2] Complete testing");
+            Console.Write("\nEnter your choice [1-2]: ");
+            char choice = (char)Console.ReadLine()[0];
+            switch (choice)
+            {
+                case '1':
+                    bool succeed = ReadAndCheck("sample.txt", 50);
+
+                    if (succeed)
+                    {
+                        Console.Write("Do you want to run Complete Testing now (y/n)? ");
+                        choice = (char)Console.Read();
+                        if (choice == 'n' || choice == 'N')
+                            break;
+                        else if (choice == 'y' || choice == 'Y')
+                            goto CompleteTest;
+                        else
+                        {
+                            Console.WriteLine("Invalid Choice!");
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+
+                case '2':
+                CompleteTest:
+                    Console.WriteLine("Complete Testing is running now...");
+                    succeed = ReadAndCheck("inout.txt", 40);
+                    if (succeed)
+                    {
+                        Console.WriteLine("\nCongratulations... your program runs successfully");
+                    }
+                    break;
+            }
+        }
+
+
+        static bool ReadAndCheck(string fileName, int timeLimit)
+		{
+            FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+			StreamReader sr = new StreamReader(file);
+			int cases = int.Parse(sr.ReadLine());
+			int wrongAnswer = 0;
+			for (int a = 0; a < cases; a++)
+			{
+				string[] twoParts = sr.ReadLine().Split(' ');
+				int nCustomers = int.Parse(twoParts[0]);
+				int nCurrency = int.Parse(twoParts[1]);
+				bool[,] knows = new bool[nCustomers,nCurrency];
+				for (int i = 0; i < nCustomers; i++)
+				{
+					string line = sr.ReadLine();
+					for (int j = 0; j < nCurrency; j++)
+					{
+						if (line[j] == 'Y')
+							knows[i, j] = true;
+						else
+							knows[i, j] = false;
+					}
+				}
+                long timeBefore = System.Environment.TickCount;        
+				for (int i = 0; i < 4000; i++)
+				{
+                    CurrencyExchange.CheckUSD(nCustomers, nCurrency, knows);
+				}
+                long timeAfter = System.Environment.TickCount;
+                if (timeAfter - timeBefore > timeLimit)
+                {
+                    Console.WriteLine("Time limit exceed: case # " + (a + 1));
+                    sr.Close();
+                    return false;
+                }
+                
+				int expectedResult = int.Parse(sr.ReadLine());
+                int receivedResult = CurrencyExchange.CheckUSD(nCustomers, nCurrency, knows);
+				if (receivedResult != expectedResult)
+				{
+					wrongAnswer++;
+					Console.WriteLine("wrong answer at case " + (a + 1) + " expected = " + expectedResult + " received = " + receivedResult);
+				}
+			}
+            sr.Close();
+            file.Close();
+            if (wrongAnswer == 0)
+            {
+                Console.WriteLine("Congratulations... :)");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(wrongAnswer + " wrong answer out of " + cases + " case with percent " + (cases - (double)wrongAnswer) * 100 / cases + "% correct");
+                return false;
+            }
+		}	
+        #endregion
+
+    }
+}
+
